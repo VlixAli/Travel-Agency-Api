7bbcca25662d4ae9625ac2dfa65c7682ee1f41c8
Ali <qali372@gmail.com>
2024-03-13 22:29:08 +0200

    build : UserController.php with store() and login()

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\LoginUserRequest;
+use App\Http\Requests\StoreUserRequest;
+use App\Http\Resources\UserResource;
+use App\Models\Role;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Response;
+
+class UserController extends Controller
+{
+    public function store(StoreUserRequest $request)
+    {
+        $user = User::create([
+            'name' => $request->validated('name'),
+            'email' => $request->validated('email'),
+            'password' => $request->validated('password'),
+        ]);
+
+        if ($request->validated('role')) {
+            $role = Role::where('name', $request->role);
+            $user->attach($role);
+        }
+
+        return new UserResource($user);
+    }
+
+    public function login(LoginUserRequest $request)
+    {
+        $user = User::where('email', $request->email)->first();
+        if (!($user && Hash::check($request->password, $user->password))) {
+            return Response::json([
+                'code' => 0,
+                'message' => 'Invalid credentials',
+            ], 401);
+        }
+        $token = $user->createToken('mentorShip access token');
+
+        return Response::json([
+            'code' => 1,
+            'token' => $token->plainTextToken,
+            'user' => $user,
+        ], 201);
+    }
+}
