70d9e5ac6054527f5b9af0d04ace6d5b630b706a
Ali <qali372@gmail.com>
2024-03-13 22:52:35 +0200

    feat : add travels/store endpoint to travel-api.php routes file

--- routes/travel-api.php
+++ routes/travel-api.php
@@ -4,3 +4,4 @@ use App\Http\Controllers\TravelController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/travels', [TravelController::class, 'index']);
+Route::post('travels/store', [TravelController::class, 'store'])->middleware(['auth:sanctum','isAdmin']);
