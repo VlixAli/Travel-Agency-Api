584533fe48dea25e0a4f716f9a61700ee78a03db
Ali <qali372@gmail.com>
2024-03-13 22:31:48 +0200

    feat : include admin-api.php in api.php routes file

--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -18,5 +19,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+Route::post('/login', [UserController::class, 'login']);
+
 include 'travel-api.php';
 include 'tour-api.php';
+include 'admin-api.php';
