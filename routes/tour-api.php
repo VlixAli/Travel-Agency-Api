04a6731382836b55f96865467fa2dbaf5db2600d
Ali <qali372@gmail.com>
2024-03-13 23:11:59 +0200

    feat : add endpoint to add tour to a travel in tour-api.php

--- routes/tour-api.php
+++ routes/tour-api.php
@@ -4,3 +4,5 @@ use App\Http\Controllers\TourController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/travels/{travel:slug}/tours', [TourController::class, 'index']);
+Route::post('/travels/{travel:slug}/tours', [TourController::class, 'store'])
+    ->middleware(['auth:sanctum', 'isAdmin']);
