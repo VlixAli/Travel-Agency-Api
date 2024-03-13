7f4645836a7b6c5dc0991baede8480f6b4342489
Ali <qali372@gmail.com>
2024-03-13 23:04:10 +0200

    feat : implement store() in TourController.php

--- app/Http/Controllers/TourController.php
+++ app/Http/Controllers/TourController.php
@@ -8,6 +8,7 @@ use App\Models\Tour;
 use App\Http\Requests\StoreTourRequest;
 use App\Http\Requests\UpdateTourRequest;
 use App\Models\Travel;
+use Carbon\Carbon;
 use Illuminate\Database\Query\Builder;
 use Illuminate\Http\Request;
 
@@ -16,7 +17,7 @@ class TourController extends Controller
     /**
      * Display a listing of the resource.
      */
-    public function index(Request $request,Travel $travel)
+    public function index(Request $request, Travel $travel)
     {
         $tours = Tour::where('travel_id', $travel->id)
             ->filter($request->query())
@@ -26,19 +27,19 @@ class TourController extends Controller
     }
 
     /**
-     * Show the form for creating a new resource.
-     */
-    public function create()
-    {
-        //
-    }
-
-    /**
      * Store a newly created resource in storage.
      */
-    public function store(StoreTourRequest $request)
+    public function store(StoreTourRequest $request, Travel $travel)
     {
-        //
+        $tour = Tour::create([
+            'travel_id' => $travel->id,
+            'name' => $request->name,
+            'starting_date' => Carbon::parse($request->starting_date),
+            'ending_date' => Carbon::parse($request->ending_date),
+            'price' => $request->price,
+        ]);
+
+        return new TourResource($tour);
     }
 
     /**
