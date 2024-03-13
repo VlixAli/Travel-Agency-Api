72aa6c0648a7cf103c08301dfed5bfb09e780faf
Ali <qali372@gmail.com>
2024-03-13 13:19:13 +0200

    build : TravelResource.php

---
+++ app/Http/Resources/TravelResource.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class TravelResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'name' => $this->name,
+            'description' => $this->description,
+            'number of days' => $this->number_of_days,
+            'number of nights' => $this->number_of_nights,
+        ];
+    }
+}
